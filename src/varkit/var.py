d4d635ea92bd5acafa09fd9a034f8f849c21d181
Andrew Carroll <carroll1301@gmail.com>
2026-08-10 13:57:27 +0100

    Updated .pytest files, added parser function

--- src/varkit/var.py
+++ src/varkit/var.py
@@ -1,6 +1,27 @@
 import pandas as pd
 import numpy as np
 
+class ParsingError(Exception):
+    '''Raise when parsing cannot work due to incorrect user input'''
+
+def parse_energi_payload(payload,currency='EUR'):
+    '''
+    Returns a pandas Series with time index in UTC and DayAheadPrice in given currency from energi filtered response.
+    Inputs: payload -> **Filtered to 1 market** .json response from request to api.energidataservice.dk/dataset/DayAheadPrices
+            currency -> column to be used: 'EUR' or 'DKK'
+    Output: pandas Series with UTC timeindex, DayAheadPrice in given currency
+    '''
+    if currency not in ['DKK','EUR']:
+        raise ParsingError(f"currency {currency} is not EUR or DKK")
+    df=pd.DataFrame(payload['records'])
+    if df.PriceArea.nunique()>1:
+        raise ParsingError(f"Payload should be filtered by 1 pricing area")
+    df['TimeUTC']=pd.to_datetime(df['TimeUTC'])
+    df=df.set_index('TimeUTC').sort_index()
+    df=df[f"DayAheadPrice{currency}"]
+    return df
+
+
 def historical_var(pnl, alpha=0.95):
     "Returns a historical VaR number using previous n days of absolute returns data"
     #Inputs: alpha -> Confidence level: 0.95 means the 95th percentile when ordered from low to high in terms of loss. 
@@ -13,9 +34,6 @@ def historical_var(pnl, alpha=0.95):
         raise ValueError(f"Need at least {min_obs} observations for {alpha:.0%} VaR, got {len(pnl)}")
     pnl = np.asarray(pnl, dtype=float) 
     pnl = pnl[~np.isnan(pnl)] 
-    if len(pnl)<5:
-        print('pnl is not long enough')  
-        return None
     sortpnl=np.sort(pnl)
     k=int(np.ceil((1-alpha)*len(pnl))-1)
     return -(sortpnl[k])
--- tests/test_var.py
+++ tests/test_var.py
@@ -1,4 +1,8 @@
-from varkit.var import historical_var
+from varkit.var import *
+import requests
+import json
+import pandas as pd
+import pytest
 
 
 def test_historical_var_hand_calculated():
@@ -11,10 +15,24 @@ def test_historical_var_hand_calculated():
 
 
 def test_alpha_monotonocity():
-    """A 80% VaR cannot be worse than a 95% VaR for the same data"""
+    """A 80% VaR cannot be greater than a 95% VaR for the same data"""
     pnl = [-10,-14,-10,20,4,5,2,18,2,3,1,-8,9,14,11,10,9,18,11,3]
 
     r1=historical_var(pnl, alpha=0.80)
     r2=historical_var(pnl, alpha=0.95)
 
-    assert r1>=r2
+    assert r1<=r2
+
+def test_market_entry():
+    """API request must filter to 1 pricing area"""
+    r=requests.get("https://api.energidataservice.dk/dataset/DayAheadPrices", params={"filter":json.dumps({"PriceArea":["DK1","DK2"]})}, timeout=30)
+    payload=r.json()
+    with pytest.raises(ParsingError,match='area'):
+        parse_energi_payload(payload,'DKK')
+
+def test_currency():
+    """API request must filter to either DKK or EURO"""
+    r=requests.get("https://api.energidataservice.dk/dataset/DayAheadPrices", params={"filter":json.dumps({"PriceArea":["DK1"]})}, timeout=30)
+    payload=r.json()
+    with pytest.raises(ParsingError,match='currency'):
+        parse_energi_payload(payload,currency='USD')
