cd475fa6663d4f60009d76bbe4f602013a122721
Andrew Carroll <carroll1301@gmail.com>
2026-08-11 11:54:11 +0100

    Add working fetch_spot_price() function

--- src/varkit/data.py
+++ src/varkit/data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+import json
 
 class ParsingError(Exception):
     '''Raise when parsing cannot work due to incorrect user input'''
@@ -15,10 +17,20 @@ def parse_energi_payload(payload,currency='EUR'):
     df=pd.DataFrame(payload['records'])
     if df.PriceArea.nunique()>1:
         raise ParsingError(f"Payload should be filtered by 1 pricing area")
-    df['TimeUTC']=pd.to_datetime(df['TimeUTC'])
+    df['TimeUTC']=pd.to_datetime(df['TimeUTC'],utc=True)
     df=df.set_index('TimeUTC').sort_index()
     df=df[f"DayAheadPrice{currency}"]
     return df
 
-def fetch_spot_price():
-    pass
+def fetch_spot_price(area, start=None, end=None, currency='EUR'):
+    """Function to return pandas DataFrame from given filters to query api.energidataservice.dk/dataset/DayAheadPrices"""
+    r=requests.get("https://api.energidataservice.dk/dataset/DayAheadPrices", 
+                   params={"filter":json.dumps({"PriceArea":area}), "start":start, "end":end}, timeout=30)
+    r.raise_for_status()
+    try:
+        payload = r.json()
+    except ValueError as err:
+        raise ParsingError(
+            f"Response from {r.url} was not JSON. First 200 chars: {r.text[:200]}"
+        ) from err
+    return parse_energi_payload(payload,currency=currency)
